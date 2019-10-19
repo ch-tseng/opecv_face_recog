fdeaef069fae609235d5d2d551cbf4ba24988285
Tseng Cheng Hsun <ch.tseng@sunplusit.com>
2019-10-19 22:21:28 +0800

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# -*- coding: UTF-8 -*-
+
 import cv2
 import imutils
 import os, time
+import numpy as np
 from sklearn.externals import joblib
+from PIL import ImageFont, ImageDraw, Image
 from libFaces import facial
 from libFaces import webCam
 from libFaces import faceRecognizer
 
+th_score = 0.45
 video_file = "videos/news1.mp4"
 cam_id = 0
 webcam_size = (1024, 768)
@@ -38,6 +43,25 @@ def exit_app():
     print("End.....")
     sys.exit(0)
 
+def printText(txt, bg, color=(0,255,0,0), size=0.7, pos=(0,0), type="Chinese"):
+    (b,g,r,a) = color
+
+    if(type=="English"):
+        ## Use cv2.FONT_HERSHEY_XXX to write English.
+        cv2.putText(bg,  txt, pos, cv2.FONT_HERSHEY_SIMPLEX, size,  (b,g,r), 2, cv2.LINE_AA)
+
+    else:
+        ## Use simsum.ttf to write Chinese.
+        fontpath = "wt009.ttf"
+        #print("TEST", txt)
+        font = ImageFont.truetype(fontpath, int(size*20))
+        img_pil = Image.fromarray(bg)
+        draw = ImageDraw.Draw(img_pil)
+        draw.text(pos,  txt, font = font, fill = (b, g, r, a))
+        bg = np.array(img_pil)
+
+    return bg
+
 if __name__ == '__main__':
     hasFrame, frame_screen, frame_org = \
         CAMERA.getFrame(rotate=cam_rotate, vflip=flip_vertical, hflip=flip_horizontal, resize=(frame_display_size[0], frame_display_size[1]))
@@ -47,12 +71,11 @@ if __name__ == '__main__':
         print(len(org_faces), len(aligned_faces), len(bbox_faces))
 
         for i, bbox in enumerate(bbox_faces):
-            cv2.rectangle(frame_org, bbox, (0,255,0), 2)
-            #print(aligned_faces[i].shape)
-            #cv2.imshow("FACE", aligned_faces[i])
-            #cv2.waitKey(0)
-            #this_embs = RG.get_embs(aligned_faces[i])
-            print(RG.verify_face(aligned_faces[i]))
+            (name, score) = RG.verify_face(aligned_faces[i])
+            if(score<th_score):
+                print(name, score)
+                cv2.rectangle(frame_org, bbox, (0,255,0), 2)
+                frame_org = printText(name, frame_org, color=(0,255,255,0), size=1.8, pos=(bbox[0],bbox[1]-10), type="Chinese")
 
         cv2.imshow("FRAME", frame_org)
         key = cv2.waitKey(1)
